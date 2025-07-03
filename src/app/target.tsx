8b6cb1963d431fd04a22f2893e7c517d5010475d
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-03 17:58:13 -0300

    feat: Criando a função handleSave

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,48 @@
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 
 import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
+import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 
 export default function Target() {
+  const [isProcessing, setProcessing] = useState(false);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState<number | null>(null);
+
+  const { id } = useLocalSearchParams<{ id?: string }>();
+
+  const handleSave = () => {
+    if (!name.trim() || !amount || amount <= 0) {
+      return Alert.alert("Atenção", "Preencha nome e valor");
+    }
+
+    setProcessing(true);
+
+    if (id) {
+    } else {
+      onCreate();
+    }
+  };
+
+  const onCreate = () => {
+    try {
+      Alert.alert("Nova menta", "Meta criada com sucesso!", [
+        {
+          text: "Ok",
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível criar a meta.");
+    } finally {
+      setProcessing(false);
+    }
+  };
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -20,10 +57,20 @@ export default function Target() {
         <Input
           label="Nome da meta"
           placeholder="Ex: Viagem para praia, Apple Watch"
+          value={name}
+          onChangeText={setName}
         />
 
-        <CurrencyInput label="Valor Alvo (R$)" value={34421.23} />
-        <Button title="Salvar" />
+        <CurrencyInput
+          label="Valor Alvo (R$)"
+          value={amount}
+          onChangeValue={setAmount}
+        />
+        <Button
+          title="Salvar"
+          onPress={handleSave}
+          isProcessing={isProcessing}
+        />
       </View>
     </View>
   );
