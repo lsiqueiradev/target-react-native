3a7c5574897cbea8e23ba0c85e2c2dcc91c1384a
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-03 13:23:06 -0300

    feat: Componente List

---
+++ src/components/List/index.tsx
@@ -0,0 +1,43 @@
+import { colors } from "@/theme";
+import {
+  FlatList,
+  FlatListProps,
+  StyleProp,
+  Text,
+  View,
+  ViewStyle,
+} from "react-native";
+import { Separator } from "../Separator";
+import { styles } from "./styles";
+
+type ListProps<T> = FlatListProps<T> & {
+  title: string;
+  emptyMessage?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+};
+
+export function List<T>({
+  title,
+  emptyMessage,
+  containerStyle,
+  data,
+  renderItem,
+  ...rest
+}: ListProps<T>) {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={styles.title}>Metas</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <Separator color={colors.gray[200]} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.listContent}
+        ListEmptyComponent={() => (
+          <Text style={styles.empty}>{emptyMessage}</Text>
+        )}
+        {...rest}
+      />
+    </View>
+  );
+}
