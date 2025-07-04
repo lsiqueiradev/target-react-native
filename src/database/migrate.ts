7c3818af476c15f3c47c33d0379a95c7027fb62b
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-04 19:49:00 -0300

    feat: correct pragma tag

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,12 +2,13 @@ import { type SQLiteDatabase } from "expo-sqlite";
 
 export async function migrate(database: SQLiteDatabase) {
   await database.execAsync(`
-      PRAGMA foregein_keys = ON;
+      PRAGMA foreign_keys = ON;
 
       CREATE TABLE IF NOT EXISTS targets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         amount FLOAT NOT NULL,
+
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp
       );
@@ -17,14 +18,14 @@ export async function migrate(database: SQLiteDatabase) {
         target_id INTEGER NOT NULL,
         amount FLOAT NOT NULL,
         observation TEXT NULL,
+
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp,
 
-        CONSTRAINT fk_targets_transactions
+        CONSTRAINT fk_transactions_target
         FOREIGN KEY (target_id) REFERENCES targets(id)
         ON DELETE CASCADE
       );
-
 
     `);
 }
